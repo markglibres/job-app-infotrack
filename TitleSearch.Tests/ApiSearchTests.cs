03fb1320559ad681e7c9bc08f698d3666c9012a6
Mark Gil Libres <markglibres@gmail.com>
2018-03-04 22:19:38 +1100

    added unit tests

---
+++ TitleSearch.Tests/ApiSearchTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TitleSearch.Api;
+using TitleSearch.Core.Services.Interface;
+using TitleSearch.Core.Utility;
+
+namespace TitleSearch.Tests
+{
+    [TestClass]
+    public class ApiSearchTests
+    {
+        [TestMethod]
+        public async Task GetRankings_ShouldReturn10Items()
+        {
+            var googleSearchService = new Mock<IGoogleSearchService>();
+            var apiController = new SearchApiController(googleSearchService.Object);
+
+            var webHelper = new WebHelper();
+            var googleQuery = new GoogleQuery(webHelper);
+
+            googleSearchService.Setup(service => service.GetRankingsAsync("", "", 100))
+                .ReturnsAsync(await googleQuery.GetRankingsAsync("test", 1));
+
+            var results = await apiController.Get("", "");
+            Assert.AreEqual(10, results.Count());
+        }
+    }
+}
---
+++ TitleSearch.Tests/GoogleQueryTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TitleSearch.Core.Utility;
+using TitleSearch.Core.Utility.Interface;
+
+namespace TitleSearch.Tests
+{
+    [TestClass]
+    public class GoogleQueryTests
+    {
+        private readonly IGoogleQuery _googleQuery;
+        private readonly IWebHelper _webHelper;
+
+        public GoogleQueryTests()
+        {
+            _webHelper = new WebHelper();
+            _googleQuery = new GoogleQuery(_webHelper);
+        }
+
+        [TestMethod]
+        public async Task GetGoogleResults_ShouldReturn10ResultItems()
+        {
+            var results = await _googleQuery.GetHtmlResults("test", 1);
+            Assert.AreEqual(10, results.Count());
+        }
+
+        [TestMethod]
+        public async Task GetGoogleUrlResult_ShouldReturnValidUrl()
+        {
+            var results = await _googleQuery.GetHtmlResults("test", 1);
+            var url = _googleQuery.GetUrlResult(results.First());
+            Assert.AreEqual(true, Uri.IsWellFormedUriString(url, UriKind.Absolute));
+        }
+    }
+}
